ae2794770b2bb64a67ecb314ac593f73f67a2ef4
rachanadas2024 <rachana.das@pumexinfotech.com>
2024-08-06 16:09:34 +0530

    feat: furniture list design

---
+++ constants/customise.ts
@@ -0,0 +1,8 @@
+
+
+export const CustomizeTitle ={
+    title: 'Customize your order',
+    subTitle:' Explore the included furniture pieces and purchase options that match your brand and selected style',
+    buttonText:'Add to cart'
+    
+}

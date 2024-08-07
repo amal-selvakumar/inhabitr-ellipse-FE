2ecc84bd69b266e164ed1b0af21e956d901556cf
dhanudhanusha99 <dhanudhanusha99@gmail.com>
2024-08-07 18:04:16 +0530

    Login API integration

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ const corsOptions = {
 export default function middleware(req: any){
 const protectedRoutes = ["/middlewareside"];
 
-    console.log('sessionStatus',sessionStatus, 'req',req)
+    // console.log('sessionStatus',sessionStatus, 'req',req)
     if(!sessionStatus && protectedRoutes.includes(req.nextUrl.pathname)) {
         const absoluteURL = new URL("/", req.nextUrl.origin);
         return NextResponse.redirect(absoluteURL.toString());
